db158bb1bc9ba7800bc79c2a2ad04d9c2da0a379
chayongbin <teddy418@nhnnext.org>
2015-05-30 23:16:54 +0900

    ADD : 카카오 로그인 Layout 붙힘. 테스트 진행중

--- app/src/main/java/com/connection/next/infantree/login/KakaoLoginActivity.java
+++
@@ -1,9 +0,0 @@
-package com.connection.next.infantree.login;
-
-import android.app.Application;
-
-/**
- * Created by chayongbin on 15. 5. 30..
- */
-public class KakaoLoginActivity extends Application {
-}
